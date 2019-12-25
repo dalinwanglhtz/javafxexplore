d453bab72f0ec41a805dfbef00bf7a1a8dffe0ab
Dale Wang <dalinwanglhtz@gmail.com>
2019-12-25 23:47:44 +1100

    added controller for getPathVariable()

---
+++ src/application/MainController.java
@@ -0,0 +1,18 @@
+package application;
+
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.TextArea;
+
+public class MainController {
+
+	@FXML
+	private TextArea displayText;
+	
+	@FXML
+	public void getPathVariable(ActionEvent event) {
+		String pathVar = System.getenv("Path");
+		displayText.setText(pathVar);
+		displayText.setWrapText(true);
+	}
+}
